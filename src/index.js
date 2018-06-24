1f8c8c4641b446fc967207768f688469838e0121
Massimo Iacolare <info@massimoiacolare.it>
2018-06-24 18:47:26 +0200

    handle unknown command

--- src/index.js
+++ src/index.js
@@ -13,4 +13,6 @@ console.log("opts: ", commandOpts)
 if (command == "echo") {
     const [message] = commandArgs
     console.log(`You're a wise man who said '${message}'`)
+} else {
+    console.log(`Unknown command '${command}'`)
 }
