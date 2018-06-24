f7e0cff8f9dfc177b4480d7092b86211cb0ddacb
Massimo Iacolare <info@massimoiacolare.it>
2018-06-24 18:44:05 +0200

    add 'echo' command

--- src/index.js
+++ src/index.js
@@ -9,3 +9,8 @@ const { _, ...commandOpts } = args
 console.log("command: ", command)
 console.log("args: ", commandArgs)
 console.log("opts: ", commandOpts)
+
+if (command == "echo") {
+    const [message] = commandArgs
+    console.log(`You're a wise man who said '${message}'`)
+}
