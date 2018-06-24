d326bf50ae27d52f754000e614f4bb6a792b4bd8
Massimo Iacolare <info@massimoiacolare.it>
2018-06-24 18:39:50 +0200

    Play with minimist

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
+const minimist = require("minimist")
 
-console.log("Hello world")
+const args = minimist(process.argv.slice(2))
+
+const [command, ...commandArgs] = args._
+const { _, ...commandOpts } = args
+
+console.log("command: ", command)
+console.log("args: ", commandArgs)
+console.log("opts: ", commandOpts)
