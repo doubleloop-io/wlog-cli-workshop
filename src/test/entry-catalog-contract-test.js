09ee7a9e5d24c03d2929a5fee88a082563aa52b9
Massimo Iacolare <info@massimoiacolare.it>
2018-06-25 19:32:34 +0200

    extract entry catalog contract tests

---
+++ src/test/entry-catalog-contract-test.js
@@ -0,0 +1,63 @@
+const { test } = require("mocha")
+const assert = require("assert")
+const assertEx = require("./assert-ex")
+const bdate = require("../core/bdate")
+
+module.exports = function(buildCatalogWith, buildEmptyCatalog) {
+    test("add one entry", async () => {
+        const newEntry = entry("entry 1", "2018-06-26")
+        const entryCatalog = buildEmptyCatalog()
+
+        await entryCatalog.add(newEntry)
+
+        const all = await entryCatalog.getAll()
+        assertEx.contains(newEntry, all)
+    })
+
+    test("get many entries", async () => {
+        const entries = [
+            entry("entry 1", "2018-06-26"),
+            entry("entry 2", "2018-06-27"),
+            entry("entry 3", "2018-06-28")
+        ]
+        const entryCatalog = buildCatalogWith(entries)
+
+        const all = await entryCatalog.getAll()
+
+        assertEx.contains(entries[0], all)
+        assertEx.contains(entries[1], all)
+        assertEx.contains(entries[2], all)
+    })
+
+    test("ordered", async () => {
+        const entries = [
+            entry("entry 2", "2018-06-27"),
+            entry("entry 1", "2018-06-26"),
+            entry("entry 3", "2018-06-28")
+        ]
+        const entryCatalog = buildCatalogWith(entries)
+
+        const all = await entryCatalog.getAll()
+
+        assert.deepEqual(all[0], entries[2])
+        assert.deepEqual(all[1], entries[0])
+        assert.deepEqual(all[2], entries[1])
+    })
+
+    test("max number of entries", async () => {
+        const entries = [
+            entry("entry 2", "2018-06-27"),
+            entry("entry 1", "2018-06-26"),
+            entry("entry 3", "2018-06-28")
+        ]
+        const entryCatalog = buildCatalogWith(entries)
+
+        const all = await entryCatalog.getAll({ maxNoOfEntries: 2 })
+
+        assert.equal(all.length, 2)
+    })
+
+    function entry(title, date) {
+        return { title: title, date: bdate.parse(date) }
+    }
+}
--- src/test/entry-catalog-inmemory-test.js
+++ src/test/entry-catalog-inmemory-test.js
@@ -1,74 +1,13 @@
-const { suite, test } = require("mocha")
-const assert = require("assert")
-const assertEx = require("./assert-ex")
-const bdate = require("../core/bdate")
+const { suite } = require("mocha")
+const contractTest = require("./entry-catalog-contract-test")
 
 const {
     makeEntryCatalog
 } = require("../infrastructure/adapters/make-inmemory-entry-catalog")
 
-/** TEST LIST
- * ---- add one entry
- * - get many entries
- * - ordered
- * - get max number of entries
- */
-
 suite("inmemory entry catalog", () => {
-    test("add one entry", async () => {
-        const newEntry = entry("entry 1", "2018-06-26")
-        const entryCatalog = makeEntryCatalog([])
+    const buildCatalogWith = entries => makeEntryCatalog(entries)
+    const buildEmptyCatalog = () => makeEntryCatalog([])
 
-        await entryCatalog.add(newEntry)
-
-        const all = await entryCatalog.getAll()
-        assertEx.contains(newEntry, all)
-    })
-
-    test("get many entries", async () => {
-        const entries = [
-            entry("entry 1", "2018-06-26"),
-            entry("entry 2", "2018-06-27"),
-            entry("entry 3", "2018-06-28")
-        ]
-        const entryCatalog = makeEntryCatalog(entries)
-
-        const all = await entryCatalog.getAll()
-
-        assertEx.contains(entries[0], all)
-        assertEx.contains(entries[1], all)
-        assertEx.contains(entries[2], all)
-    })
-
-    test("ordered", async () => {
-        const entries = [
-            entry("entry 2", "2018-06-27"),
-            entry("entry 1", "2018-06-26"),
-            entry("entry 3", "2018-06-28")
-        ]
-        const entryCatalog = makeEntryCatalog(entries)
-
-        const all = await entryCatalog.getAll()
-
-        assert.deepEqual(all[0], entries[2])
-        assert.deepEqual(all[1], entries[0])
-        assert.deepEqual(all[2], entries[1])
-    })
-
-    test("max number of entries", async () => {
-        const entries = [
-            entry("entry 2", "2018-06-27"),
-            entry("entry 1", "2018-06-26"),
-            entry("entry 3", "2018-06-28")
-        ]
-        const entryCatalog = makeEntryCatalog(entries)
-
-        const all = await entryCatalog.getAll({ maxNoOfEntries: 2 })
-
-        assert.equal(all.length, 2)
-    })
+    contractTest(buildCatalogWith, buildEmptyCatalog)
 })
-
-function entry(title, date) {
-    return { title: title, date: bdate.parse(date) }
-}
