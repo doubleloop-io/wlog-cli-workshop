dfbcf9abe47d95b5b7b17d6088801b7531cad663
Massimo Iacolare <info@massimoiacolare.it>
2018-06-25 12:20:54 +0200

    entry catalog refactoring to avoid aliasing issues

--- src/infrastructure/adapters/make-inmemory-entry-catalog.js
+++ src/infrastructure/adapters/make-inmemory-entry-catalog.js
@@ -1,7 +1,10 @@
 function makeEntryCatalog(entries = []) {
+    const clone = obj => ({ ...obj })
+    const innerEntries = entries.map(clone)
+
     return {
         async getAll() {
-            return Promise.resolve(entries)
+            return Promise.resolve([...innerEntries])
         }
     }
 }
